3d4ffb6a09b407249fecd9bac884da0de895ab93
Kcct1227 <106064654+Kcct1227@users.noreply.github.com>
2022-05-23 17:06:34 +0630

    Add files via upload

---
+++ Starter/Utils/Router.swift
@@ -0,0 +1,55 @@
+//
+//  Router.swift
+//  Starter
+//
+//  Created by KC on 11/02/2022.
+//
+
+import Foundation
+import UIKit
+
+enum StoryBoardName: String{
+    case Main = "Main"
+    case Authentication = "Authentication"
+    case LaunchScreen = "LaunchScreen"
+}
+
+extension UIStoryboard{
+    static func mainStoryBoard()-> UIStoryboard{
+        UIStoryboard(name: StoryBoardName.Main.rawValue, bundle: nil)
+    }
+}
+
+extension UIViewController{
+    func navigateToMovieDetailViewController(movieId: Int, contentType: String){
+        guard let vc = UIStoryboard.mainStoryBoard().instantiateViewController(identifier: MovieDetailViewController.identifer) as? MovieDetailViewController else {return}
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .flipHorizontal
+        let viewModel = MovieDetailsViewModel()
+        viewModel.movieID = movieId
+        viewModel.type = contentType
+        vc.viewModel = viewModel
+        self.navigationController?.pushViewController(vc, animated: true)
+        //present(vc, animated: true)
+        
+    }
+    
+    func navigateToSearchContentViewController(){
+        let vc = RxSearchViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+        //present(vc, animated: true, completion: nil)
+    }
+    
+    func navigateToMoreShowCaseViewController(data: [MovieResult]){
+        let vc = MoreShowCaseViewController()
+        vc.initData = data
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func navigateToMoreActorsViewController(data: [ActorInfoResponse]){
+        let vc = MoreActorViewController()
+        vc.initData = data
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
